7e2948ed4b1b6b7c0c671a51476ab1a4278f33cd
doebi <christoph@doeberl.at>
2017-03-07 23:37:09 +0100

    new interlaced thumbnails

--- photos/generateThumbnails.sh
+++ photos/generateThumbnails.sh
@@ -2,6 +2,6 @@ for dir in ./*/
 do
     cd $dir
     mkdir thumbnails
-    mogrify -thumbnail 400x400^ -gravity center -extent 400x400 -format png -path thumbnails *.png
+    mogrify -thumbnail 400x400^ -gravity center -extent 400x400 -format png -interlace plane -path thumbnails *.png
     cd ..
 done
