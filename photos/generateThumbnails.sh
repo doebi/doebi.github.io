8f7d87e449d642527c549b380dee643f0dacadec
doebi <christoph@doeberl.at>
2017-09-12 07:56:39 +0200

    fixed thumbnail script

--- photos/generateThumbnails.sh
+++ photos/generateThumbnails.sh
@@ -1,7 +1,13 @@
 for dir in ./*/
 do
     cd $dir
-    mkdir thumbnails
-    mogrify -thumbnail 400x400^ -gravity center -extent 400x400 -format png -interlace plane -path thumbnails *.png
+    if [ -d "./thumbnails" ]; then
+        echo "skipping $dir"
+    else
+        echo "generating thumbnails for $dir"
+        mkdir thumbnails
+        mogrify -thumbnail 400x400^ -gravity center -extent 400x400 -format png -interlace plane -path thumbnails *.png
+        mogrify -thumbnail 400x400^ -gravity center -extent 400x400 -format jpg -interlace plane -path thumbnails *.jpg
+    fi
     cd ..
 done
